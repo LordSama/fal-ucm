1ea669b36d82d91977b25e07e72eed6e51d37d2d
LordSama <72462333+LordSama@users.noreply.github.com>
2022-06-02 11:52:15 +0200

    Add files via upload

---
+++ Acepta El Reto/116 Hola Mundo/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    
+    cin >> n;
+    if (!std::cin)
+        return false;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Hola mundo." << endl;
+    }
+
+    return true;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso());
+
+
+
+    return 0;
+}
